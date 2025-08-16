b2542924bcf466515b45cef0fa6562300548f92c
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2025-08-16 18:16:48 +0530

    solved a problem in set

---
+++ python/crio_sheet/set/contains_duplicates.py
@@ -0,0 +1,9 @@
+#LINK: https://leetcode.com/problems/contains-duplicate/
+
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        #add elements to set and compare lengths
+        return len(set(nums)) != len(nums)
+#TC: O(N) : set() function traverses each element of nums
+#SC: O(N) : set object stores all elements of nums in worst case
