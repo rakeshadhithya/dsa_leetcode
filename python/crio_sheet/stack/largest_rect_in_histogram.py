9a726857a82fe70c8db80f6108a8af59bccb373e
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2025-09-14 21:48:39 +0530

    largest rect in histogram

---
+++ python/crio_sheet/stack/largest_rect_in_histogram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        max_area = 0
+        stack = []
+        heights.append(0) #sentinal(guard), to remove all left over in stack when no more heights
+        for i, h in enumerate(heights):
+            #if current height is less, then stack top height(larger) ends
+            while len(stack) != 0 and h < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if len(stack) == 0 else i - stack[-1] - 1
+                max_area = max( max_area, height * width )
+            stack.append(i)
+        return max_area
